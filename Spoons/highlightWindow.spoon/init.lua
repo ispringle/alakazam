bea4fddd1a707b69c44679bb3bec576bb95fcac7
Ian S. Pringle <ipringle@jackhenry.com>
2024-01-09 14:11:46 -0600

    moved to subscriber method for watching focus change

--- Spoons/highlightWindow.spoon/init.lua
+++ Spoons/highlightWindow.spoon/init.lua
@@ -4,52 +4,60 @@ M = {
 }
 
 function M:outlineWindow(window)
-            self.currentOutline = hs.canvas.new(window:frame()):appendElements(
-               {
-                  action = "stroke", padding = 4, type = "rectangle",
-                  strokeColor = { hex = self.color },
-                  strokeWidth = 4,
-                  strokeJoinStyle = "round",
-                  withShadow = true,
-                }):show()
+   self.currentOutline = hs.canvas.new(
+      window:frame()):appendElements({
+         action = "stroke", padding = 4, type = "rectangle",
+         strokeColor = { hex = self.color },
+         strokeWidth = 4,
+         strokeJoinStyle = "round",
+         withShadow = true
+      }):show()
 end
 
 function M:blinkWindow(window)
-            hs.canvas.new(window:frame()):appendElements({
-                  action = "fill", padding = 4, type = "rectangle",
-                  fillColor = { alpha = 0.1, hex = self.color },
-                                                                },
-               {
-                  action = "stroke", padding = 4, type = "rectangle",
-                  strokeColor = { hex = self.color },
-                  strokeWidth = 8,
-                  strokeJoinStyle = "round",
-                  withShadow = true,
-               }):show():delete(1)
+   hs.canvas.new(
+      window:frame()):appendElements(
+      {action = "fill",
+       padding = 4,
+       type = "rectangle",
+       fillColor = { alpha = 0.1, hex = self.color }},
+      {action = "stroke",
+       padding = 4,
+       type = "rectangle",
+       strokeColor = { hex = self.color },
+       strokeWidth = 8,
+       strokeJoinStyle = "round",
+       withShadow = true}
+  ):show():delete(1)
 end
 
 function M:clearOutline()
    pcall(function() self.currentOutline:delete() end)
 end
 
+local winFocus = hs.window.filter.default
+local focused = hs.window.filter.windowFocused
+local moved = hs.window.filter.windowMoved
+
 function M:focusChanged()
-   local focusWatcher = hs.application.watcher.new(function (name, event, obj)
+   winFocus:subscribe(
+      focused,
+      function (window, name, event)
          if (name == "Raycast") then return end
-         if (event == hs.application.watcher.activated) then
-            self:clearOutline()
-            local focusedWindow = obj:focusedWindow()
-            self:outlineWindow(focusedWindow)
-            self:blinkWindow(focusedWindow)
-         end
-   end)
-focusWatcher:start()    
+         print(name)
+         self:clearOutline()
+         self:outlineWindow(window)
+         self:blinkWindow(window)
+      end)
 end
 
 function M:windowMoved()
-   hs.window.filter.default:subscribe(hs.window.filter.windowMoved, function(focusedWindow, name, event)
-                                 self:clearOutline()
-                                 self:outlineWindow(focusedWindow)
-   end)
+   winFocus:subscribe(
+      moved,
+      function(focusedWindow, name, event)
+         self:clearOutline()
+         self:outlineWindow(focusedWindow)
+      end)
 end
 
 function M:start()
